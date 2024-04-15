861f78d85b99638659c8c3e0364a4858100352ac
Aditya Nijasure <31492782+adnijasure@users.noreply.github.com>
2024-04-15 16:23:55 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#data = pd.read_csv('/Users/adityanijasure/Desktop/NFL Data/nfloffenseweek3.csv')
+
+#print(data.isna().any())
+
+#data = data.set_index('team')
+
+#passing = data.drop(columns=['rank','games','offensive_plays','yards_per_play','fumbles_lost','turnovers_lost','1st_downs','rushing_attempts'])
+#print(passing.iloc[:])
+#passing_yards_average = passing.groupby('team')['passing_yards'].mean()
+
+#passing_yards_average.plot(kind = 'bar')
+#plt.show()
